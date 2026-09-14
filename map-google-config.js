3b9e24352033f61ca5511677e039e3363006add2
ffunnyggang <ffunnyggang@gmail.com>
2026-09-15 01:29:51 +0900

    Update map-google-config.js

--- map-google-config.js
+++ map-google-config.js
@@ -2,4 +2,4 @@
  * Google Cloud Console에서 발급한 브라우저 API 키를 아래 문자열에 입력하세요.
  * 예: window.FUNY_GOOGLE_MAPS_API_KEY='AIza...';
  */
-window.FUNY_GOOGLE_MAPS_API_KEY='';
+window.FUNY_GOOGLE_MAPS_API_KEY='AIzaSyDBSPRs1jnfePKN9ZvZuyUWvlreMeAXYmw';

1536679fe39fe5c86861d09d80cf6f267d3560a5
ffunnyggang <ffunnyggang@gmail.com>
2026-09-16 14:22:23 +0900

    Bust Naver clustering cache

--- map-country-v1.js
+++ map-country-v1.js
@@ -5,7 +5,7 @@
   core.async=false;
   core.onload=()=>{
     const cluster=document.createElement('script');
-    cluster.src='map-cluster-v1.js?v=20260916-1419';
+    cluster.src='map-cluster-v1.js?v=20260916-1423';
     cluster.async=false;
     document.body.appendChild(cluster);
   };

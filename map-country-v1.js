7b5c197510129c8f574afef9e73f3845448fb261
ffunnyggang <ffunnyggang@gmail.com>
2026-09-16 22:49:22 +0900

    Refresh non-interactive cluster touch fix cache

--- map-country-v1.js
+++ map-country-v1.js
@@ -5,7 +5,7 @@
   core.async=false;
   core.onload=()=>{
     const cluster=document.createElement('script');
-    cluster.src='map-cluster-v1.js?v=20260916-2242';
+    cluster.src='map-cluster-v1.js?v=20260916-2251';
     cluster.async=false;
     document.body.appendChild(cluster);
   };

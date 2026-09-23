b5b0b3a71dbf2b5f192de5c6b655eaca1c652711
ffunnyggang <ffunnyggang@gmail.com>
2026-09-23 23:01:16 +0900

    Refresh map country module cache

--- map-country-v1.js
+++ map-country-v1.js
@@ -1,7 +1,7 @@
 /* FUNY PIN country module loader */
 (function(){
   const core=document.createElement('script');
-  core.src='map-country-core-v1.js?v=20260916-2238';
+  core.src='map-country-core-v1.js?v=20260923-2300';
   core.async=false;
   core.onload=()=>{
     const cluster=document.createElement('script');
